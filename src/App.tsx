fdf2d4eda7fbc45b24565cbb8c0e0e118e156f79
Raphael <phael92br@gmail.com>
2023-08-28 21:49:54 -0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,80 @@
-import { Header } from "./components/header"
-import { Aluno } from "./components/aluno"
-import { Footer } from "./components/footer"
+import { useState} from "react"
+
+
+interface infoAlunoProps {
+  nome: string;
+  idade: string;
+}
+
+
 
 export default function App () {
+  const [input, setInput] = useState("");
+  const [idade, setIdade] = useState("");
+  
+  const [infoAluno, setInfoAluno] = useState<infoAlunoProps>()
+
+  const [contador, setContador] = useState(0)
+  
+  
+  
+  function aumentar () {
+    setContador(valorAtual => valorAtual + 1);
+  }
+
+  function diminuir () {
+    if (contador == 0) {
+      return;
+    }
+
+    setContador(valorAtual => valorAtual -1)
+  }
+
+  function mostrarAluno() {  
+    
+    setInfoAluno ({
+      nome: input,
+      idade: idade,
+    })
+    
+       
+  }
+
   return (
     <div>
-      <Header />
+      <h1>Conhecendo useState</h1>
 
-      <Aluno nome="Raphael Almeida" idade = {31}/>
+      <input 
+      placeholder="Digite o nome"
+      value={input}
+      onChange={ (e) => setInput(e.target.value)}
+      />
 
-      <Footer/>
+      <br />
+      <br />
+
+      <input  
+      placeholder="Digite sua idade aqui"
+      value={idade}
+      onChange={(e) => setIdade(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={mostrarAluno}>Mostrar Aluno e Idade</button>
+
+      <hr />
+
+      <h3>Bem Vindo : {infoAluno?.nome}</h3>
+      <h3>Idade: {infoAluno?.idade}</h3>
+
+      <hr />
+      <br />
+      <h1>Contador com useState</h1>
+      <button onClick={aumentar}>+</button> {contador} <button onClick={diminuir}>-</button>
     </div>
+    
   )
 }
 
--- src/components/footer.tsx
+++
@@ -1,5 +0,0 @@
-export function Footer () {
-    return (
-        <h5 className="footer">CopyRight: Raphael - 2023</h5>
-    )
-}
